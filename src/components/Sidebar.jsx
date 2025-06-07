20d04b7919b02940dbceb0a40bd04c46de86eb01
saba-ramezani <saba.rad.sr@gmail.com>
2025-06-07 21:41:34 +0330

    fixed the position of sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,72 +11,74 @@ import BedtimeIcon from '@mui/icons-material/Bedtime';
 const SideBar = () => {
   return (
     <Box flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}} position={"sticky"}>
-      <List>
-        <ListItem>
-          <ListItemButton component="a" href='#home'>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#pages'>
-            <ListItemIcon>
-              <ArticleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Pages" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#groups'>
-            <ListItemIcon>
-              <GroupIcon />
-            </ListItemIcon>
-            <ListItemText primary="Groups" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#market'>
-            <ListItemIcon>
-              <StorefrontIcon />
-            </ListItemIcon>
-            <ListItemText primary="Market" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#friends'>
-            <ListItemIcon>
-              <Diversity1Icon />
-            </ListItemIcon>
-            <ListItemText primary="Friends" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#settings'>
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href='#profile'>
-            <ListItemIcon>
-              <AccountCircleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <BedtimeIcon />
-            </ListItemIcon>
-            <Switch />
-          </ListItemButton>
-        </ListItem>
-      </List>
+      <Box position={"fixed"}>
+        <List>
+          <ListItem>
+            <ListItemButton component="a" href='#home'>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#pages'>
+              <ListItemIcon>
+                <ArticleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#groups'>
+              <ListItemIcon>
+                <GroupIcon />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#market'>
+              <ListItemIcon>
+                <StorefrontIcon />
+              </ListItemIcon>
+              <ListItemText primary="Market" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#friends'>
+              <ListItemIcon>
+                <Diversity1Icon />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#settings'>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href='#profile'>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <BedtimeIcon />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   )
 }
